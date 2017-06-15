e14bf4debca381affa7b4b556d6ddd193db6d457
maslowalex <thunderbozza@gmail.com>
2017-06-15 18:24:19 +0300

    Creating seeds and rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ end
 
 puts "10 blog posts created"
 
-5.ties do |skill|
+5.times do |skill|
   Skill.create!(
     title: "Rails #{skill}",
     percent: 15
@@ -17,9 +17,9 @@ end
 9.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: ,
-    body: ,
-    main_image: ,
-    thumb_image:
+    subtitle: "My great service",
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    main_image: "http://via.placeholder.com/400x600",
+    thumb_image: "http://via.placeholder.com/350x150"
     )
 end

18441ed5a82ee091b92fdaa821fac68a6923f49c
maslowalex <thunderbozza@gmail.com>
2017-08-22 10:29:15 +0300

    Integrated defatul images to portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,8 +1,17 @@
 class Portfolio < ApplicationRecord
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :thumb_image
 
   def self.angular
     where(subtitle: "Angular")
+  end
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on rails") }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.thumb_image ||= "http://via.placeholder.com/350x150"
+    self.main_image ||= "http://via.placeholder.com/400x600"
   end
 end 
 
